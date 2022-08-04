30c56d93e12609d15f11bd3db452f8e314208e38
max <adimax2953@gmail.com>
2022-08-04 12:13:12 +0800

    workpool新增

--- kafkatool/kafkaManager_test.go
+++ kafkatool/kafkaManager_test.go
@@ -3,8 +3,11 @@ package kafkatool_test
 import (
 	"testing"
 
+	gotool "github.com/adimax2953/go-tool"
 	"github.com/adimax2953/go-tool/kafkatool"
 )
+
+var c kafkatool.KafkaConfig
 
 func Test_SendtoKafka(t *testing.T) {
 
@@ -15,23 +18,25 @@ func Test_SendtoKafka(t *testing.T) {
 		ReplicationFactor: 1,
 	}
 	config.CreateTopic("test3", 10)
-
+	c = *config
 	// m := map[string]string{}
 	// for i := 0; i < 10000; i++ {
 	// 	m[gotool.IntToStr(i)+"@player"] = "value " + gotool.IntToStr(i)
 	// }
 	// config.WriteMessagesKeyValue("test3", m)
-	config.ReadMessages("test3", "121")
+	//config.ReadMessages("test3", "1")
 
 	//config.GetTopic()
 	//config.DelTopic(config.GetTopic()...)
-
-	// for i := 0; i < 10000; i++ {
-	// 	m := map[string]string{}
-
-	// 	m[gotool.IntToStr(i)+"@once"] = gotool.IntToStr(i)
-	// 	config.WriteMessagesKeyValue("test001", m)
-	// }
+	wp := gotool.NewWorkPool(1)
+	p, _ := wp.NewWorkPoolWithFunc(100, func(i interface{}) {
+		test(i)
+	})
+	for i := 0; i < 10000; i++ {
+		p.Invoke(i)
+	}
+	defer wp.Release()
+	defer p.Release()
 
 	// s := make([]string, 10000)
 	// for i := 0; i < 10000; i++ {
@@ -42,5 +47,11 @@ func Test_SendtoKafka(t *testing.T) {
 	//config.CreateTopic("test1112")
 	//config.CreateConn("test12")
 	//config.WriteMessages("test3", "da", "da", "der", "ma", "te", "sen")
+}
+func test(i interface{}) {
+	m := map[string]string{}
+
+	m[gotool.IntToStr(i.(int))+"@once"] = gotool.IntToStr(i.(int))
+	c.WriteMessagesKeyValue("test3", m)
 
 }
