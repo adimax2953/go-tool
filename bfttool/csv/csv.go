0877dcfb7b70c36902b8df62aa7092af480a23a6
max.tsai <adimax2953@gmail.com>
2023-10-11 10:31:54 +0800

    csv 製作

---
+++ bfttool/csv/csv.go
@@ -0,0 +1,42 @@
+package csv
+
+import (
+	"bytes"
+	"encoding/csv"
+
+	LogTool "github.com/adimax2953/log-tool"
+)
+
+func CreateCSVWithHeader(header []string, body [][]string) ([]byte, error) {
+
+	// 创建一个内存中的缓冲区来存储CSV数据
+	var buf bytes.Buffer
+
+	// 创建一个 CSV writer，将数据写入到缓冲区中
+	writer := csv.NewWriter(&buf)
+
+	// 刷新并关闭 CSV writer
+	defer writer.Flush()
+
+	// 写入 CSV 头部
+	if err := writer.Write(header); err != nil {
+		LogTool.LogError("写入 CSV 头部时出错:", err)
+		return nil, err
+	}
+
+	// 逐行写入数据
+	for _, record := range body {
+		if err := writer.Write(record); err != nil {
+			LogTool.LogError("写入 CSV 数据时出错:", err)
+			return nil, err
+		}
+	}
+
+	// 检查是否有错误
+	if err := writer.Error(); err != nil {
+		LogTool.LogError("CSV 写入错误:", err)
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
+}
